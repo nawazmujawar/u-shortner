14e0ea5abfc2bda4feb29b1b78069d19518ba532
Nawaz Mujawar <nawazgmujawar21@gmail.com>
2020-07-11 18:27:59 +0530

    Updates

---
+++ precache-manifest.a972462af321a7e3b076ccf30867f367.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0da16e6025796a0916b7a85e3d6af27",
+    "url": "/u-shortner/index.html"
+  },
+  {
+    "revision": "195492bb58cb38efbe47",
+    "url": "/u-shortner/static/css/main.190ac8e1.chunk.css"
+  },
+  {
+    "revision": "f9960cf2e12dabb9f772",
+    "url": "/u-shortner/static/js/2.40405a1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/u-shortner/static/js/2.40405a1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "195492bb58cb38efbe47",
+    "url": "/u-shortner/static/js/main.6c8deda8.chunk.js"
+  },
+  {
+    "revision": "753ca33828900eb00ead",
+    "url": "/u-shortner/static/js/runtime-main.470b3ffc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/u-shortner/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "07b653bd0e12aa0ba8fefa0e53b1fe89",
+    "url": "/u-shortner/static/media/portable-document-format.07b653bd.svg"
+  }
+]);
